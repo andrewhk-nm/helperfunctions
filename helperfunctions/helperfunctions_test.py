c4c5829d1f258d60c4815fd3cc9d94f929b0790e
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-02-08 13:16:31 -0500

    commented out a PythonChallenge test

--- helperfunctions/helperfunctions_test.py
+++ helperfunctions/helperfunctions_test.py
@@ -8,21 +8,22 @@ from functools import namedtuple
 
 import helperfunctions as hf
 
-class Test_PythonChallenge_test1(unittest.TestCase):
-    known_answers_gen_char_neighbor = [('U', 'kAeMKENi'),
-                                       ('K', 'AewULNih'),
-                                       ('L', 'ewtKEihr')
-                                       ]
-    
-    def test_HelperFunctions_gen_char_neighbor_known_answers(self):
-        """ Test some known answers of the generator.
-        """
-        test_gen = hf.gen_char_neighbor('problem3_source_mess.txt')
-        for center, neighbors in self.known_answers_gen_char_neighbor:
-            print('center={}, neighbors={}'.format(center, neighbors))
-            test_ans = next(test_gen)
-            print('test_ans={}'.format(test_ans))
-            self.assertEqual((center, neighbors), test_ans)
+## This should go in the python challenge test module
+##class Test_PythonChallenge_test1(unittest.TestCase):
+##    known_answers_gen_char_neighbor = [('U', 'kAeMKENi'),
+##                                       ('K', 'AewULNih'),
+##                                       ('L', 'ewtKEihr')
+##                                       ]
+##    
+##    def test_HelperFunctions_gen_char_neighbor_known_answers(self):
+##        """ Test some known answers of the generator.
+##        """
+##        test_gen = hf.gen_char_neighbor('problem3_source_mess.txt')
+##        for center, neighbors in self.known_answers_gen_char_neighbor:
+##            print('center={}, neighbors={}'.format(center, neighbors))
+##            test_ans = next(test_gen)
+##            print('test_ans={}'.format(test_ans))
+##            self.assertEqual((center, neighbors), test_ans)
 
 class Test_testtest(unittest.TestCase):
     def test_A(self):
