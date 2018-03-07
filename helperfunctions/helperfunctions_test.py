2f037b7fdef076f1b9a7044cd1e962d9251fb0e5
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2018-03-07 14:04:25 -0500

    cleaning up after separation

--- helperfunctions/helperfunctions_test.py
+++ helperfunctions/helperfunctions_test.py
@@ -8,23 +8,6 @@ from functools import namedtuple
 
 import helperfunctions as hf
 
-## This should go in the python challenge test module
-##class Test_PythonChallenge_test1(unittest.TestCase):
-##    known_answers_gen_char_neighbor = [('U', 'kAeMKENi'),
-##                                       ('K', 'AewULNih'),
-##                                       ('L', 'ewtKEihr')
-##                                       ]
-##    
-##    def test_HelperFunctions_gen_char_neighbor_known_answers(self):
-##        """ Test some known answers of the generator.
-##        """
-##        test_gen = hf.gen_char_neighbor('problem3_source_mess.txt')
-##        for center, neighbors in self.known_answers_gen_char_neighbor:
-##            print('center={}, neighbors={}'.format(center, neighbors))
-##            test_ans = next(test_gen)
-##            print('test_ans={}'.format(test_ans))
-##            self.assertEqual((center, neighbors), test_ans)
-
 class Test_PythonProjectEuler_problem11_and_Helper(unittest.TestCase):
     """ Collection of Tests focusing on problem11 and the HelperFunctions 
     functions that I may create there.
